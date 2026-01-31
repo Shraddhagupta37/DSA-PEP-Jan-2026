d4cc5953599e3222bae20584ec37d3b35a0db46c
Shraddhagupta37 <gshraddha246850@gmail.com>
2026-01-31 09:26:59 +0530

    Save

--- DSA/recursion/arraySearch.cpp
+++ DSA/recursion/arraySearch.cpp
@@ -10,15 +10,15 @@ bool findKey(int arr[], int size, int key) {
 }
 
 bool findKeyBinary(int arr[], int l, int r, int key) {
-    if(l>=r) return false;
+    if(l>r) return false;
 
     int mid = (l+r)/2;
     if(arr[mid] == key)
         return true;
     else if (arr[mid] < key)
-        return findKeyBinary(arr, l+1, r, key);
+        return findKeyBinary(arr, mid+1, r, key);
     else
-        return findKeyBinary(arr, l, r-1, key);    
+        return findKeyBinary(arr, l, mid-1, key);    
 }
 
 int main() {
@@ -26,5 +26,7 @@ int main() {
     int size = sizeof(arr)/sizeof(arr[0]);
     cout << findKey(arr, size, 8) << endl;
 
-    cout << findKeyBinary(arr, 0, size-1, 8) << endl;
+    int arr2[] = {2,9,12,36,41,82};
+    size = sizeof(arr2)/sizeof(arr2[0]);
+    cout << findKeyBinary(arr2, 0, size-1, 12) << endl;
 }
