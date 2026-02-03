d4bdb046312113db26452d1fcedca211d11e78a9
Shraddhagupta37 <gshraddha246850@gmail.com>
2026-02-03 10:34:45 +0530

    Save

--- DSA/linked list/01.cpp
+++ DSA/linked list/01.cpp
@@ -54,6 +54,59 @@ void insertAtBegin(Node* &head, int val) {
     head = newNode;
 }
 
+void insertAtPos(Node* head,int val, int p) {
+    if(head == NULL && p>1)
+        return;
+    
+    Node* newNode = new Node(val);
+    if(p==1) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    for(int i=1; i<p-1; i++) {
+        current = current->next;
+
+        if(current == NULL) {   //if p is > length of LL + 1, do nothing
+            cout << "Position out of bounds" << endl;
+            return;
+        }
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void deleteAtBegin(Node* &head) {
+    if(head == NULL)
+        return;
+
+    Node* temp = head;
+    head = head->next;
+    delete(temp);
+}
+
+void deleteAtEnd(Node* &head) {
+    if(head == NULL) 
+        return;
+    
+    if(head->next == NULL) {
+        Node* temp = head;
+        head = NULL;
+        delete(temp);
+        return;
+    }
+    
+    Node* current = head;
+    while(current->next->next != NULL) {
+        current = current->next;
+    }
+    Node* temp = current->next;
+    current->next = NULL;
+    delete(temp);
+}
+
 int main() {
     //static way to create LL
     // Node* n1 = new Node(1);
@@ -68,16 +121,47 @@ int main() {
     int n;
     cout << "Enter no. of elements of LL: ";
     cin >> n;
-    cout << "Enter elements of LL: " << endl;
 
     //insertion at end
-    Node* head = NULL;
+    cout << "Enter elements of LL1 (insert at end): " << endl;
+    Node* head1 = NULL;
     for(int i=1;i<=n;i++) {
         int val;
         cin >> val;
-        insertAtEnd(head,val);
+        insertAtEnd(head1,val);
     }
+    printList(head1);
 
-    printList(head);
+
+    //insert at begining
+    cout << "Enter elements of LL2 (insert at begining): " << endl;
+    Node* head2 = NULL;
+    for(int i=1;i<=n;i++) {
+        int val;
+        cin >> val;
+        insertAtBegin(head2,val);
+    }
+    printList(head2);
+
+    
+    //insert at position
+    int e, p;
+    cout << "Enter element & position to insert node in LL1: ";
+    cin >> e >> p;
+    insertAtPos(head1, e, p);
+    cout << "After trying insertion at given pos: ";
+    printList(head1);
+
+
+    //deletion at end
+    deleteAtEnd(head1);
+    cout << "LL1 after deleting last node: ";
+    printList(head1);
+
+
+    //deletion at begining
+    deleteAtBegin(head2);
+    cout << "LL2 after deleting first node: ";
+    printList(head2);
 
 }
