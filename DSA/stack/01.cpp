ec86cad7f145a04a9d5baea6ad32b108cc3fc0a2
Shraddhagupta37 <gshraddha246850@gmail.com>
2026-02-09 12:38:06 +0530

    Save

--- DSA/stack/01.cpp
+++ DSA/stack/01.cpp
@@ -1,17 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//using array
+class myStack {
+    public:
+    int* arr;
+    int top;
+    int size;
+
+    myStack(int size) {
+        top = -1;
+        arr = new int[size];
+        this->size = size;
+    }
+
+    bool isEmpty() {
+        return (top == -1); 
+    }
+
+    bool isFull() {
+        if(top >= size) return true;
+        else return false;
+    }
+
+    void push(int val) {
+        if(top+1 == size) {
+            cout << "Stack overflow" << endl;
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    void pop() {
+        if(top == -1) {
+            cout << "Stack underflow" << endl;
+            return;
+        }
+        top--;
+    }
+
+    int peek() {
+        if(top == -1) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return arr[top];
+    }
+};
+
 int main() {
-    stack<int> st;
+    myStack st(5);
     st.push(1);
     st.push(2);
     st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
 
-    cout << st.top() << endl;
     st.pop();
-    cout << st.top() << endl;
+    cout << st.peek() << endl;
 
-    cout << st.size() << endl;
-    cout << (st.empty() ? "Empty" : "Not Empty") << endl;
-    
+    //printing stack elements
+    while(!st.isEmpty()) {
+        cout << st.peek() << " ";
+        st.pop();
+    }
 }
