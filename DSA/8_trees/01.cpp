743a158489a3963058dbec94b9c296fc2f559d51
Shraddhagupta37 <gshraddha246850@gmail.com>
2026-02-16 16:37:01 +0530

    Save

--- DSA/8_trees/01.cpp
+++ DSA/8_trees/01.cpp
@@ -14,6 +14,24 @@ Terminology:
     6. Subtree: any node of a tree, with all of its descendents
     7. Level: refers to its distance from the root of the tree; the root is generally level 0
     8. balanced tree: a binary tree is balanced if for every node in the tree, the difference in height between its left and right subtrees is at most 1
+    9. Height -> the maximum level in a tree determines its height, the height of a node in a tree is the length of the longest path from that node to a leaf node
+    10. Depth -> the depth of the node is the number of nodes along the path from the root to that node
+
+Properties of Binary Tree --->
+    1. if h = height of the binary tree, then
+        a -> maximum number of leaves = 2^h 
+        b -> maximum number of nodes = 2^(h+1)-1
+    2. if a binary tree contains m nodes at level l, it contains at most 2m nodes at level l+1
+    3. since a binary tree can contain at most 1 node at level 0(root), it can contain at most 2^l nodes at level l
+    4. the total number of edges in a full binary tree with n nodes is n-1 edges
+
+Types --->
+    1. Full binary tree -> a tree of height h has all its leaves at level h, means all non leaf nodes of a full binary tree has two children, and the leaf node have no children
+    2. Complete binary tree -> all levels are filled except the last level, the last level should be filled from left to right
+    3. Skew binary tree -> tilted to any one side only -> left skewed binary tree and right skewed binary tree
+
+
+A binary tree is balanced if for every node the height diff b/w its left & right subtree is st most one
 */
 
 class Node {
